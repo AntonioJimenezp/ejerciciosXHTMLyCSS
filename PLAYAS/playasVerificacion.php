e28751711e01dc1dd3b710c8fce40dd9e6ceef57
Antonio <tonysponsor@hotmail.com>
2018-04-10 09:50:34 +0200

    php acabao

--- PLAYAS/playasVerificacion.php
+++ PLAYAS/playasVerificacion.php
@@ -1,27 +1,36 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="ISO-8859-1">
-<title>Sexo</title>
-<link href="estilo3.css" rel="stylesheet" type="text/css" title="Color" />
+    <meta charset="ISO-8859-1">
+    <title>Verificacion datos</title>
+    <link href="Estilos/estiloPlaya.css" rel="stylesheet" type="text/css" title="Color" />
 </head>
+
 <body>
 
-<?php
+    <?php
 
-
-if (isset($_POST["opinion"])) {
-    $dato = $_POST["opinion"];
- if ($dato == 1) {
-    echo 'Eres un <strong>hombre</strong>';
-} else {
-    echo 'Eres una <strong>mujer</strong>';
+if(isset($_POST["opinion"])){
+    $valor = $_POST["opinion"];
+    if($valor == "si")
+        print '<p>Ha estado en la playa</p>';
+    elseif ($valor == "no")
+        print '<p>No ha estado en la playa</p>';
+    else
+        print '<p>Visitará la playa</p>';
+}else{
+    print '<p>No ha indicado si le gusta la playa</p>';
 }
-} else {
-    echo 'No ha marcado su sexo';
-}
 
+if (isset($_POST['valor'])) {
+   print '<p>Datos enviados</p>';
+
+}else{
+    print '<p>No ha introducido apartado</p>';
+}
 ?>
-
+        <p><a href="SanLorenzo.html">Volver a la pagina San Lorenzo</a></p>
 </body>
+
 </html>
